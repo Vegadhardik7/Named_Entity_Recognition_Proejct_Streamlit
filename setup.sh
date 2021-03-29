8f57b7a1497e8321fb17cb520701115b939359e9
Vegadhardik <vegadhardik7@gmail.com>
2021-03-29 16:03:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-mkdir -p ~/.stramlit/
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"vegadhardik7@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
-port = $PORT\n\
-enableCORS = false\n\
 headless = true\n\
-\n\
-" > ~/ .streamlit/config.toml
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
